2ad9dad6aa2faeb435fa260f1ced4ca5cab266e6
Aksaev <mail-ax@ya.ru>
2022-10-19 21:22:37 +0400

    Урок 2.1 ООП. Домашнее задание - Human. Выполнил.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Human maksim = new Human("Максим", "Минск", 35,"бренд-менеджер");
+        Human ania = new Human("Аня", "Москва", 29, "методист образовательных программ");
+        Human katya = new Human("Катя", "Калининград", 28, "продакт-менеджер");
+        Human artem = new Human("Артем", "Москва", 27, "директор по развитию бизнеса");
+
+        System.out.println("Урок 2.1 ООП. Домашнее задание - Human.");
+
+        System.out.println(maksim);
+        System.out.println(ania);
+        System.out.println(katya);
+        System.out.println(artem);
+
     }
 }
